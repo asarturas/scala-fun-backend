dbcf040f8b09d19c24f5fee9a2a6e8a630077222
Arturas Smorgun <arturas@spikerlabs.com>
2017-05-07 00:53:49 +0100

    Add json sample

--- build.sbt
+++ build.sbt
@@ -5,3 +5,4 @@ version := "1.0"
 scalaVersion := "2.12.2"
 
 libraryDependencies += "com.typesafe.akka" %% "akka-http" % "10.0.6"
+libraryDependencies += "com.typesafe.akka" %% "akka-http-spray-json" % "10.0.4"
--- src/main/scala/WebServer.scala
+++ src/main/scala/WebServer.scala
@@ -1,11 +1,31 @@
+import akka.Done
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model._
 import akka.http.scaladsl.server.Directives._
 import akka.stream.ActorMaterializer
+
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
+import spray.json.DefaultJsonProtocol._
+
+import scala.concurrent.Future
 import scala.io.StdIn
 
 object WebServer {
+
+  // domain model
+  final case class Item(name: String, id: Long)
+  final case class Order(items: List[Item])
+
+  // formats for unmarshalling and marshalling
+  implicit private val itemFormat = jsonFormat2(Item)
+  implicit private val orderFormat = jsonFormat1(Order)
+
+  import scala.concurrent.ExecutionContext.Implicits.global
+  // (fake) async database query api
+  def fetchItem(itemId: Long): Future[Option[Item]] = Future(Some(Item(s"Random $itemId", itemId)))
+  def saveOrder(order: Order): Future[Done] = Future(Done)
+
   def main(args: Array[String]) {
 
     implicit val system = ActorSystem("my-system")
@@ -18,7 +38,26 @@ object WebServer {
         get {
           complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "<h1>Say hello to akka-http</h1>"))
         }
-      }
+      } ~ get {
+        pathPrefix("item" / LongNumber) { id =>
+          // there might be no item for a given id
+          val maybeItem: Future[Option[Item]] = fetchItem(id)
+
+          onSuccess(maybeItem) {
+            case Some(item) => complete(item)
+            case None       => complete(StatusCodes.NotFound)
+          }
+        }
+      } ~ post {
+          path("create-order") {
+            entity(as[Order]) { order =>
+              val saved: Future[Done] = saveOrder(order)
+              onComplete(saved) { done =>
+                complete("{true}")
+              }
+            }
+          }
+        }
 
     val bindingFuture = Http().bindAndHandle(route, "localhost", 8080)
 
