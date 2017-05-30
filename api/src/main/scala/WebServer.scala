b467cb76d889d2510ca70d6f4ca1a1390cdfcde9
Arturas Smorgun <arturas@spikerlabs.com>
2017-05-30 18:00:37 +0100

    Update web server to use the new ways

--- api/src/main/scala/WebServer.scala
+++ api/src/main/scala/WebServer.scala
@@ -6,13 +6,19 @@ import akka.http.scaladsl.server.{MethodRejection, RejectionHandler}
 import akka.stream.ActorMaterializer
 import akka.util.Timeout
 import com.typesafe.config.ConfigFactory
+import fun.scala.actors.Messages.{RandomVideo, ReturnRandomVideo}
 import fun.scala.pocket.actors.PocketClient
-import fun.scala.{PocketAdapter, ReturnRandomVideo, Video, VideoRepository}
+import fun.scala.store.video.Video
+//import fun.scala.{PocketAdapter, ReturnRandomVideo, Video, VideoRepository}
 import akka.pattern.ask
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.model.StatusCodes._
 import ch.megard.akka.http.cors.scaladsl.CorsDirectives._
-import fun.scala.video.VideoPostProcessor
+import fun.scala.actors.Messages.CollectVideos
+import fun.scala.actors.{Processor, Sourcer, Storage}
+import fun.scala.processors.UrlProcessor
+import fun.scala.sourcers.{PocketConfig, PocketSourcer}
+//import fun.scala.video.VideoPostProcessor
 
 import scala.concurrent.Future
 import scala.concurrent.duration._
@@ -27,7 +33,7 @@ object WebServer {
 
   def main(args: Array[String]) {
 
-    implicit val system = ActorSystem("my-system")
+    implicit val system = ActorSystem("scala-fun")
     implicit val materializer = ActorMaterializer()
 
     implicit def rejectionHandler =
@@ -47,13 +53,28 @@ object WebServer {
     implicit val executionContext = system.dispatcher
 
     // app actors
-    val videoRepository = system.actorOf(Props[VideoRepository], "videoRepository")
-    val videoPostProcessor = system.actorOf(VideoPostProcessor.withProps(videoRepository), "videoPostProcessor")
-    val pocketAdapter = system.actorOf(PocketAdapter.withProps(videoPostProcessor), "pocketAdapter")
+    val (consumerKey, accessToken) = (config.getString("pocketConsumerKey"), config.getString("pocketAccessToken"))
+    val sourcer = system.actorOf(
+      Sourcer.create(PocketSourcer.Pocket, PocketConfig(consumerKey, accessToken)),
+      "pocket-sourcer"
+    )
+    system.actorOf(
+      Processor.create(new UrlProcessor()),
+      "url-processor"
+    )
+    val storage = system.actorOf(
+      Props[Storage],
+      "storage"
+    )
+    sourcer ! CollectVideos()
+
+//    val videoRepository = system.actorOf(Props[VideoRepository], "videoRepository")
+//    val videoPostProcessor = system.actorOf(VideoPostProcessor.withProps(videoRepository), "videoPostProcessor")
+//    val pocketAdapter = system.actorOf(PocketAdapter.withProps(videoPostProcessor), "pocketAdapter")
 
     // pocket
-    val (consumerKey, accessToken) = (config.getString("pocketConsumerKey"), config.getString("pocketAccessToken"))
-    system.actorOf(PocketClient.withProps(consumerKey, accessToken, pocketAdapter), "pocketClient")
+//    val (consumerKey, accessToken) = (config.getString("pocketConsumerKey"), config.getString("pocketAccessToken"))
+//    system.actorOf(PocketClient.withProps(consumerKey, accessToken, pocketAdapter), "pocketClient")
 
     // circe for decoding responses in json
     import de.heikoseeberger.akkahttpcirce.FailFastCirceSupport._
@@ -63,7 +84,10 @@ object WebServer {
       path("next") {
         get {
           implicit val timeout: Timeout = 5.seconds
-          val video: Future[Video] = (videoRepository ? ReturnRandomVideo).mapTo[Video]
+          val video: Future[fun.scala.Video] = (storage ? ReturnRandomVideo()).map {
+            case RandomVideo(v) =>
+              fun.scala.Video(v.title, v.embedUrl.toString, v.likesOverall, v.playsOverall)
+          }
           complete(video)
         }
       }
@@ -73,8 +97,8 @@ object WebServer {
 
     println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
     StdIn.readLine() // let it run until user presses return
-    bindingFuture
-      .flatMap(_.unbind()) // trigger unbinding from the port
-      .onComplete(_ => system.terminate()) // and shutdown when done
+//    bindingFuture
+//      .flatMap(_.unbind()) // trigger unbinding from the port
+//      .onComplete(_ => system.terminate()) // and shutdown when done
   }
 }
