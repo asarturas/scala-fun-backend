1ba5dc155ff1ff1436905f30100d60af92b1d4cb
Arturas Smorgun <arturas@spikerlabs.com>
2017-05-30 19:16:33 +0100

    Add script to build api client npm package

--- build-api-client.sh
+++ build-api-client.sh
@@ -1,1 +1,7 @@
 #!/usr/bin/env bash
+sbt "apiClientJs/fullOptJS"
+cd api-client-js
+cp -f target/scala-2.12/api-client-js-opt.js src/main/resources/lib/index.js
+cd src/main/resources
+rm -f scala-fun-*.tgz
+npm pack
