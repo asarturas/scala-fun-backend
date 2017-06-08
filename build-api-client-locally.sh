1269c940c859f76c55bbfdc42492390aea2db438
Arturas Smorgun <arturas@spikerlabs.com>
2017-06-08 16:58:06 +0100

    Add a script to build scala-fun-client npm package locally

--- build-api-client-locally.sh
+++ build-api-client-locally.sh
@@ -2,11 +2,13 @@
 sbt "apiClientJs/fullOptJS"
 export SCALA_API_VERSION=$(sbt -no-colors --info ";project apiClientJs; version" | tail -1 | sed 's/\[info\][^0-9a-zA-Z]*//' | sed 's/\-[a-zA-Z].*$//')
 cd api-client-js
-mkdir src/main/resources/lib
-cp -f target/scala-2.12/api-client-js-opt.js src/main/resources/lib/index.js
-cd src/main/resources
+mkdir -p src/main/resources/localbuild/lib
+cp -f target/scala-2.12/api-client-js-opt.js src/main/resources/localbuild/lib/index.js
+cd src/main/resources/localbuild/
+cp ../index.d.ts .
+cp ../index.js .
+sed "s/%%VERSION%%/$SCALA_API_VERSION/" ../package.template.json > package.json
 rm -f scala-fun-client*.tgz
-sed "s/%%VERSION%%/$SCALA_API_VERSION/" package.template.json > package.json
 npm pack
-cd ../../../../
-ls api-client-js/src/main/resources/scala-fun-client*.tgz
+cd ../../../../../
+ls api-client-js/src/main/resources/localbuild/scala-fun-client*.tgz
