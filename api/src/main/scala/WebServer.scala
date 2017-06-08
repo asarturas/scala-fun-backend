e1e9a110967205a951b3cabf6fd093626128aa53
Arturas Smorgun <arturas@spikerlabs.com>
2017-06-08 16:57:36 +0100

    Use configurable host/port, instead of hardcoded ones

--- api/src/main/scala/WebServer.scala
+++ api/src/main/scala/WebServer.scala
@@ -78,9 +78,13 @@ object WebServer {
       }
     }
 
-    val bindingFuture = Http().bindAndHandle(route, "localhost", 8080)
 
-    println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
+    val apiHost = config.getString("apiServerHost")
+    val apiPort = config.getInt("apiServerPort")
+
+    val bindingFuture = Http().bindAndHandle(route, apiHost, apiPort)
+
+    println(s"Server online at http://${apiHost}:${apiPort}/\nPress RETURN to stop...")
     StdIn.readLine() // let it run until user presses return
     bindingFuture
       .flatMap(_.unbind()) // trigger unbinding from the port
