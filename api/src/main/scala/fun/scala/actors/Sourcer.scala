c0b75fd36c84643fe8be47d3974f8da176fa9a05
Arturas Smorgun <arturas@spikerlabs.com>
2017-08-02 23:32:40 +0100

    Pass actor refernces on construction instead of doing selection in receive loop

--- api/src/main/scala/fun/scala/actors/Sourcer.scala
+++ api/src/main/scala/fun/scala/actors/Sourcer.scala
@@ -1,22 +1,23 @@
 package fun.scala.actors
 
-import akka.actor.{Actor, ActorLogging, Props}
+import akka.actor.{Actor, ActorLogging, ActorRef, Props}
 import fun.scala.actors.Messages.{CollectVideos, ProcessSourcedVideos}
 import fun.scala.data.Source
 import fun.scala.sourcers.{PocketConfig, PocketSourcer, SourcerConfig}
 import fun.scala.sourcers.PocketSourcer.Pocket
 
 object Sourcer {
-  def create(source: Source, config: SourcerConfig): Props = Props(new Sourcer(source, config))
+  def create(source: Source, config: SourcerConfig, processor: ActorRef): Props =
+    Props(new Sourcer(source, config, processor))
 }
 
-class Sourcer(source: Source, config: SourcerConfig) extends Actor with ActorLogging {
+class Sourcer(source: Source, config: SourcerConfig, processor: ActorRef) extends Actor with ActorLogging {
   val sourcer: fun.scala.sourcers.Sourcer = (source, config) match {
     case (Pocket, PocketConfig(consumerKey, accessToken)) => new PocketSourcer(consumerKey, accessToken)
   }
   def receive: Receive = {
     case CollectVideos() =>
       log.info("sourcer received a request to collect stuff")
-      context.system.actorSelection("user/processor") ! ProcessSourcedVideos(sourcer.collect())
+      processor ! ProcessSourcedVideos(sourcer.collect())
   }
 }
